7da7d0c0d8e7193c67a0602e2bac6f17e4248f4e
Shun Cheung <shunkongcheung@gmail.com>
2020-11-02 16:47:40 -0500

    feat: typescript and src setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default MyApp;
