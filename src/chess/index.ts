7c480b7e7851d164f2f3a59c7671a54bde3fda02
Shun Cheung <shunkongcheung@gmail.com>
2020-11-02 23:10:53 -0500

    feat(chess): export getIsPositionEmpty

--- src/chess/index.ts
+++ src/chess/index.ts
@@ -3,9 +3,11 @@ import getInitialBoard from "./getInitialBoard";
 import getMovedBoard from "./getMovedBoard";
 import getPieceNextPositions from "./getPieceNextPositions";
 import getWinner from "./getWinner";
+import getIsPieceEmpty from "./getIsPieceEmpty";
 
 export {
   getAllNextPositions,
+  getIsPieceEmpty,
   getInitialBoard,
   getMovedBoard,
   getPieceNextPositions,
