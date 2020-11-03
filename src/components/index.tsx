47af04d4b7aab5e1fd7d14a79bcf6ee9404fc7ba
Shun Cheung <shunkongcheung@gmail.com>
2020-11-03 17:41:59 -0500

    feat(components): more componentes

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,7 @@
+import Button from "./Button";
 import ChessBoard from "./ChessBoard";
 import ChessPiece from "./ChessPiece";
+import ListItem from "./ListItem";
+import ListItemGroup from "./ListItemGroup";
 
-export { ChessBoard, ChessPiece };
+export { Button, ChessBoard, ChessPiece, ListItem, ListItemGroup };
