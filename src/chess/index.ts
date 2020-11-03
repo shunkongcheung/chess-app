5b84946558efbc4d1ff84baa57d00079a9e6272e
Shun Cheung <shunkongcheung@gmail.com>
2020-11-03 15:51:38 -0500

    feat(chess): export getHashFromBoard

--- src/chess/index.ts
+++ src/chess/index.ts
@@ -1,5 +1,6 @@
 import getAllNextPositions from "./getAllNextPositions";
 import getBoardWinnerAndScore from "./getBoardWinnerAndScore";
+import getHashFromBoard from "./getHashFromBoard";
 import getInitialBoard from "./getInitialBoard";
 import getMovedBoard from "./getMovedBoard";
 import getPieceNextPositions from "./getPieceNextPositions";
@@ -8,6 +9,7 @@ import getIsPieceEmpty from "./getIsPieceEmpty";
 export {
   getAllNextPositions,
   getBoardWinnerAndScore,
+  getHashFromBoard,
   getIsPieceEmpty,
   getInitialBoard,
   getMovedBoard,
