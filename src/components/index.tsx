9765e62ed8597687a9c1cc12bd80418669882205
Shun Cheung <shunkongcheung@gmail.com>
2020-11-03 21:38:20 -0500

    feat(MoveListItem): init

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,13 @@ import ChessBoard from "./ChessBoard";
 import ChessPiece from "./ChessPiece";
 import ListItem from "./ListItem";
 import ListItemGroup from "./ListItemGroup";
+import MoveListItem from "./MoveListItem";
 
-export { Button, ChessBoard, ChessPiece, ListItem, ListItemGroup };
+export {
+  Button,
+  ChessBoard,
+  ChessPiece,
+  ListItem,
+  ListItemGroup,
+  MoveListItem,
+};
