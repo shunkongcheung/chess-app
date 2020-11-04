89554a8d1f132b2f2a369860aec82f4fbae2e759
Shun Cheung <shunkongcheung@gmail.com>
2020-11-03 22:33:03 -0500

    feat(ChessMlModel): init

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,7 @@
 import ChessBoard from "./ChessBoard";
+import ChessMlModel from "./ChessMlModel";
 import ChessMove from "./ChessMove";
 import GameSeries from "./GameSeries";
 import MoveSequence from "./MoveSequence";
 
-export { ChessBoard, ChessMove, GameSeries, MoveSequence };
+export { ChessBoard, ChessMlModel, ChessMove, GameSeries, MoveSequence };
