9ecb6d6fb250979e48c9ecbcec9f1254f28cea2c
Shun Cheung <shunkongcheung@gmail.com>
2020-11-03 21:43:17 -0500

    feat(ListItemGroup): height and overflow

--- src/components/ListItemGroup/index.tsx
+++ src/components/ListItemGroup/index.tsx
@@ -5,6 +5,8 @@ const Container = styled.ul`
   list-style-type: none;
   margin-top: 0;
   padding: 0;
+  overflow: auto;
+  height: 100%;
 `;
 
 interface ListItemGroupProps extends React.HTMLAttributes<HTMLUListElement> {}
