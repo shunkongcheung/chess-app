628f74c47d5287d82c5e418da63657a254d51a48
Shun Cheung <shunkongcheung@gmail.com>
2020-12-04 18:29:14 -0500

    feat(GameSeries): migrations add isPlayBy

--- src/entities/GameSeries.ts
+++ src/entities/GameSeries.ts
@@ -1,4 +1,4 @@
-import { PrimaryGeneratedColumn, Entity, OneToMany } from "typeorm";
+import {Column, PrimaryGeneratedColumn, Entity, OneToMany } from "typeorm";
 
 import MoveSequence from "./MoveSequence";
 
@@ -21,6 +21,12 @@ import MoveSequence from "./MoveSequence";
 class GameSeries {
   @PrimaryGeneratedColumn()
   id: number;
+
+	@Column({default: true})
+	botPlayerIsComp:boolean;
+
+	@Column({default: true})
+	topPlayerIsComp:boolean;
 
   @OneToMany(() => MoveSequence, (moveSequence) => moveSequence.gameSeries)
   moveSequences: Array<MoveSequence>;
