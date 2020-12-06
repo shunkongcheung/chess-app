cffb20facb9c0ef0d4bc6d804ecafb3e493ee852
Shun Cheung <shunkongcheung@gmail.com>
2020-12-06 17:25:56 -0500

    feat(game-series/create): calculateQScore

--- src/routes/game-series/create.ts
+++ src/routes/game-series/create.ts
@@ -17,6 +17,8 @@ import {
   MoveSequence,
 } from "../../entities";
 
+import { calculateQScore } from "../../utils";
+
 import { choose } from "../chess-ml";
 
 type Board = Array<Array<string>>;
@@ -63,7 +65,7 @@ const create = async (connection: Connection, query: Query) => {
     where: { gameSeries },
     order: { id: "DESC" },
   });
-  await calculateQScoreHelper(connection, moveSequences);
+  await calculateQScore(connection, moveSequences);
 
   // return series
   return gameSeries;
@@ -171,60 +173,6 @@ const helper = async (
     randomRate,
     side
   );
-};
-
-const calculateQScoreHelper = async (
-  connection: Connection,
-  moveSequences: Array<MoveSequence>
-) => {
-  const moveSequence = moveSequences.shift();
-  if (!moveSequence) return;
-
-  const [ALPHA, GAMMA] = [0.1, 0.8];
-  const { chessMove } = moveSequence;
-  const isUpperSide = chessMove.side === Side.Top;
-  const nextSide = isUpperSide ? Side.Bottom : Side.Top;
-
-  const movedBoard = chessMove.toBoard.board;
-
-  const chessBoard = await connection
-    .getRepository(ChessBoard)
-    .findOne({ board: movedBoard });
-
-  const opponantChessMoves = await connection
-    .getRepository(ChessMove)
-    .createQueryBuilder("chessmove")
-    .leftJoinAndSelect("chessmove.fromBoard", "fromBoard")
-    .where(`"fromBoard"."id" = :id`, {
-      id: chessBoard?.id || -1,
-    })
-    .andWhere(`"chessmove"."side" = :side`, { side: nextSide })
-    .getMany();
-
-  const scores = opponantChessMoves.length
-    ? opponantChessMoves.map((chessMove) => chessMove.qScore)
-    : [0];
-
-  // the best score that i get aftr i move the board
-  const optimalFutureReward = -Math.max(...scores);
-
-  // populate qscores
-  const { qScore: oQScore } = chessMove;
-  let reward = chessMove.toBoard.simpleScore;
-  if (!isUpperSide) reward = -reward;
-
-  const predictValue = opponantChessMoves.length ? optimalFutureReward : reward;
-
-  // adding predict value to existing value, if existing value is not zero, this
-  // will accumulate as this spot is being visited more and more
-  chessMove.qScore = predictValue + 0.01 * oQScore;
-
-  // const addValue = ALPHA * (reward + GAMMA * optimalFutureReward - oQScore);
-  // chessMove.qScore += addValue;
-
-  await connection.getRepository(ChessMove).save(chessMove);
-
-  return calculateQScoreHelper(connection, moveSequences);
 };
 
 export default create;
